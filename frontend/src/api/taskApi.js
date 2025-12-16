0e02c995ab2208f2e837cc29dcca6cffcc4bd10f
Aniket Singh <aniketsingh7141340@gmail.com>
2025-12-16 23:07:33 +0530

    correct backend url

--- frontend/src/api/taskApi.js
+++ frontend/src/api/taskApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://task-manager-web-app-2.onrender.com/",
+  baseURL: "https://task-manager-web-app-2.onrender.com/",
 });
 
 export default API;
